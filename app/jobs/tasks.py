9bdbc6fa16ee535693e929e96daf43b78eeb663f
skuantaiuly <skuanataiuly@gmail.com>
2023-12-20 04:05:29 +0600

    Updated tasks

--- app/jobs/tasks.py
+++ app/jobs/tasks.py
@@ -8,20 +8,19 @@ celery = Celery('tasks', broker=settings.broker_url)
 
 
 def create_cron_task(name: str, cron: dict, func, args: list[str] | None = None):
-    celery.conf.beat_schedule[name] = {
-        'task': func,
-        'schedule': crontab(minute=cron['minute'], hour=cron['hour'], day_of_week=cron['day_of_week']),
-        'args': args
-    }
+    celery.add_periodic_task(
+        crontab(minute=cron['minute'], hour=cron['hour'], day_of_week=cron['day_of_week']),
+        func.s(*args),
+        name=name
+    )
 
 
 def create_periodic_task(name: str, seconds: int, func, args: list[str] | None = None):
-    minutes = seconds // 60
-    celery.conf.beat_schedule[name] = {
-        'task': func,
-        'schedule': crontab(minute=f'*/{minutes}'),
-        'args': args
-    }
+    celery.add_periodic_task(
+        seconds,
+        func.s(*args),
+        name=name
+    )
 
 
 def delete_task(name: str):
